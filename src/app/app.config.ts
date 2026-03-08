3118a42a534d85223a65d8904f4d2119aaa16dbc
Diego-8520 <diego.alejandor.8520@gmail.com>
2026-03-08 14:20:51 -0500

    feat: cambio de nombre de firebase.ts a enviorenment.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-import { environment } from '../environments/firebase';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
