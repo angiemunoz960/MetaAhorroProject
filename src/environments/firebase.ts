02e8ef013d2b4ee274301183821352b95fd82b8d
Diego-8520 <diego.alejandor.8520@gmail.com>
2026-03-08 13:59:37 -0500

    Initial setup: Angular 21 + Firebase + Chart.js + Bootstrap

---
+++ src/environments/firebase.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA_Fp1k1ho5NWvPhs1riVzGLrUh83Tw0ME',
+    authDomain: 'metaahorroproject.firebaseapp.com',
+    projectId: 'metaahorroproject',
+    storageBucket: 'metaahorroproject.firebasestorage.app',
+    messagingSenderId: '1033385399110',
+    appId: '1:1033385399110:web:91b7a43c2ecd0a7e7f4b47',
+    measurementId: 'G-ZZJ1L1LRQL',
+  },
+};
